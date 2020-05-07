39963a97d41e63ecbfc2f1d234f5fa52c9045dad
Louis Allemann <36743745+jeanlouison@users.noreply.github.com>
2020-05-07 20:05:13 +0200

    added useSwipe custom hook

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { get_today, get_by_date } from '../../api/ade';
+import { get_by_date } from '../../api/ade';
+import useSwipe from '../../hooks/useSwipe';
 
 import Card from './Card';
 
@@ -15,18 +16,28 @@ const Calendar = () => {
     let [ curDate, setCurDate ] = useState(
         `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
         );
-    curDate = '2020-03-04'
+    // curDate = '2020-03-04'
     let [ courses, setCourses ] = useState([{}]);
+    const page = useRef(null);
 
     useEffect(() => {
         get_by_date(group, curDate)
         .then(c => setCourses(c))
-    }, [])
+    }, [group, curDate]);
 
-    return <>
+    useSwipe(page, {
+        onSwipeLeft: event => {
+            setCurDate('left');
+        },
+        onSwipeRight: event => {
+            setCurDate('right');
+        }
+    }, 30);
+
+    return <div ref={page}>
         <h1>{curDate}</h1>
-        {courses.map(c => <Card id={c.start} course={c} />)}
-    </>
+        {courses.map(c => <Card key={c.start} course={c} />)}
+    </div>
 };
 
 export default Calendar;
---
+++ src/hooks/useSwipe.js
@@ -0,0 +1,79 @@
+import { useState, useEffect, useRef } from 'react';
+
+const useSwipe = (ref, handlers, minDelta) => {
+
+    const element = ref.current;
+
+    let [ gesture, setGesture ] = useState('');
+    let [ touches, setTouches ] = useState(null)
+    let initialTouches = useRef(null);
+
+    useEffect(() => {
+    
+        const handleTouchStart = event => {
+            const currentTouches = getCurrentTouches(event, event.touches);
+            setTouches(currentTouches);
+            initialTouches.current = currentTouches;
+        };
+    
+        const handleTouchMove = event => {
+            const currentTouches = getCurrentTouches(event, event.changedTouches)
+            setTouches(currentTouches);
+
+            if (Math.abs(currentTouches.deltaX < minDelta) && currentTouches.x < touches.x) {
+                setGesture('onSwipeLeft')
+            }
+            else if (Math.abs(currentTouches.deltaX < minDelta) && currentTouches.x > touches.x) {
+                setGesture('onSwipeRight');
+            }
+            else {
+                setGesture('');
+            }
+        };
+    
+        const handleTouchEnd = event => {
+            if (gesture) callHandler(gesture, event);
+        };
+
+        const callHandler = (eventName, event) => {
+            if (eventName && handlers[eventName] && typeof handlers[eventName] === 'function') {
+              handlers[eventName](event);
+            }
+        };
+
+        
+        const getCurrentTouches = (originalEvent, touches) => {
+            
+            const firstTouch = initialTouches.current;
+            const pointer = {
+                x: touches[0].clientX,
+                y: touches[0].clientY
+            };
+
+            return {
+                preventDefault: originalEvent.preventDefault,
+                ...pointer,
+                deltaX: firstTouch ? pointer.x - firstTouch.x : 0
+            };
+        };
+    
+        if (element !== null) {
+         
+            element.addEventListener('touchstart', handleTouchStart);
+            element.addEventListener('touchmove', handleTouchMove);
+            element.addEventListener('touchend', handleTouchEnd);
+        }
+
+        return () => {
+            if (element !== null) {
+                element.removeEventListener('touchstart', handleTouchStart);
+                element.removeEventListener('touchmove', handleTouchMove);
+                element.removeEventListener('touchend', handleTouchEnd);
+            }
+        };
+    });
+};
+
+
+export default useSwipe;
+
